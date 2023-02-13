8c2cee990fe62d6f8ed6a9f5463cc4a211e7a3aa
SaroarShahan <saroar.shahan@gmail.com>
2023-02-14 00:27:43 +0600

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+import styles from '~/styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Shopping Cart with NextJS and TypeScript</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h2>Hello World</h2>
+      </main>
+    </>
+  );
+}
